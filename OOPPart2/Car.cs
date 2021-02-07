a0afeabef21bf00383148ce57c068d50f6260637
Jedimik <krajca.tomascz@gmail.com>
2021-02-07 20:19:43 +0100

    ghfghfhfg

---
+++ OOPPart2/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPart2
+{
+    public class Car
+    {
+        public string Barva { get; set; }
+        public string Znacka { get; set; }
+        public int Year { get; set; }
+
+        public Car(string barva, string znacka, int year)
+        {
+            Barva = barva;
+            Znacka = znacka;
+            Year = year;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ OOPPart2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auticko = new Car("zelena","oppel",2004);
+            Car auticko2 = new Car("modra", "oppel", 2004);
+            Car auticko3 = new Car("zluta", "oppel", 2004);
+            Console.WriteLine(auticko.Barva + ", " + auticko2.Barva + ", " + auticko3.Barva);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
